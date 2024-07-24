da0129cd91ba2c91a0e4c643dd50670b4c673b63
carmenflorsl <164540909+carmenflorsl@users.noreply.github.com>
2024-07-24 08:19:54 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -7,23 +7,56 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class Users(Base):
+    __tablename__ = 'users'
+    id_user = Column(Integer, primary_key=True)
+    nombre = Column(String(250), nullable=False)
+    apellido = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False, unique= True)
+    passw = Column(String(250), nullable=False, unique= True)
+    fecha_subscripcion= Column(String(250))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    #favoritos = relationship('Favoritos', back_populates='user')
+
+class Favoritos(Base):
+    __tablename__ = 'favoritos'
+    id_favorito = Column(Integer, primary_key=True)
+    id_user = Column(Integer, ForeignKey('users.id_user'), nullable=False)
+    id_planetas = Column(Integer, ForeignKey('planetas.id_planeta'), nullable=False)
+    id_vehiculos = Column(Integer, ForeignKey('vehiculos.id_vehiculo'), nullable=False)
+    id_personajes = Column(Integer, ForeignKey('personajes.id_personaje'), nullable=False)
+
+    #user = relationship('Users', back_populates='favoritos')
+
+class Planetas(Base):
+    __tablename__ = 'planetas'
+    id_planeta = Column(Integer, primary_key=True)
+    nombre = Column(String(250), nullable=False, unique= True)
+    diametro = Column(Integer)
+    poblacion = Column(Integer)
+    clima = Column(String(250))
+    #id_user= Column(Integer, ForeignKey('users.id_user'))
+
+class Personajes(Base):
+    __tablename__ = 'personajes'
+    id_personaje = Column(Integer, primary_key=True)
+    nombre = Column(String(250), nullable=False, unique= True)
+    genero = Column(String(250))
+    altura = Column(Integer)
+    peso = Column(Integer)
+    #id_user= Column(Integer, ForeignKey('users.id_user'))
+
+class Vehiculos(Base):
+    __tablename__ = 'vehiculos'
+    id_vehiculo = Column(Integer, primary_key=True)
+    nombre = Column(String(250), nullable=False, unique= True)
+    modelo = Column(String(250))
+    fabricante = Column(String(250))
+    num_pasajeros = Column(Integer)
+    costo = Column(Integer)
+    #id_user= Column(Integer, ForeignKey('users.id_user'))
+
+
 
     def to_dict(self):
         return {}
